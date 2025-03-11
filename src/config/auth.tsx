4282dec4474281ac614345f8a20e261ae6a51c24
katorivn699 <anhkhung006@gmail.com>
2025-03-11 13:06:45 +0700

    fix redirectUrl

--- src/config/auth.tsx
+++ src/config/auth.tsx
@@ -1,5 +1,13 @@
 const CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID;
-const REDIRECT_URI = "http://localhost:3000/api/auth/callback";
+
+const getBaseUrl = () => {
+  if (typeof window !== 'undefined') {
+    return window.location.origin;
+  }
+  return process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'; // Fallback cho SSR
+};
+
+const REDIRECT_URI = `${getBaseUrl()}/api/auth/callback`;
 
 export const DISCORD_AUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
   REDIRECT_URI
